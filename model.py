f5c7e39b4efc0414d65b7bf314bc091f7832e143
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-02-22 01:12:27 -0500

    test

---
+++ model.py
@@ -0,0 +1,24 @@
+from micrograd.embedding import InputEmbedding
+from micrograd.pos_encoding import PositionalEncoding
+from micrograd.multi_head_attention import MultiHeadAttentionBlock
+from micrograd.feedforward import FeedForward
+from micrograd.encoder import EncoderBlock, Encoder
+from micrograd.decoder import DecoderBlock, Decoder
+from micrograd.projection_layer import ProjectionLayer
+from micrograd.transformer import Transformer
+
+def build_transformer(src_vocab_size, tgt_vocab_size, src_seq_len, tgt_seq_len, d_model, nhead, dim_feedforward, num_encoder_layers, num_decoder_layers, dropout):
+    src_embed = InputEmbedding(d_model, src_vocab_size)
+    tgt_embed = InputEmbedding(d_model, tgt_vocab_size)
+    src_pos = PositionalEncoding(d_model, src_seq_len)
+    tgt_pos = PositionalEncoding(d_model, tgt_seq_len)
+    multi_head_attn = MultiHeadAttentionBlock(d_model, nhead, dropout)
+    feed_forward = FeedForward(d_model, dim_feedforward, dropout)
+    encoder_block = EncoderBlock(multi_head_attn, feed_forward, dropout)
+    encoder = Encoder([encoder_block for _ in range(num_encoder_layers)])
+    decoder_multi_head_attn = MultiHeadAttentionBlock(d_model, nhead, dropout)
+    decoder_feed_forward = FeedForward(d_model, dim_feedforward, dropout)
+    decoder_block = DecoderBlock(multi_head_attn, decoder_multi_head_attn, decoder_feed_forward, dropout)
+    decoder = Decoder([decoder_block for _ in range(num_decoder_layers)])
+    projection_layer = ProjectionLayer(d_model, tgt_vocab_size)
+    return Transformer(encoder, decoder, src_embed, tgt_embed, src_pos, tgt_pos, projection_layer)
