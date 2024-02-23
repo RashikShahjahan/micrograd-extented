66235ac9d9ba82ba88f00fe24e1c744cc858fc0f
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-02-22 19:07:32 -0500

    test

--- micrograd/blocks/pos_encoding.py
+++ micrograd/blocks/pos_encoding.py
@@ -11,8 +11,8 @@ class PositionalEncoding(nn.Module):
         self.dropout = Dropout(dropout)
 
         pe = torch.zeros((seq_len, d_model))
-        position = torch.arange(0, seq_len).reshape(-1, 1)
-
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) # (seq_len, 1)
+        
         div_term = torch.exp(torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
 
         pe[:, 0::2] = torch.sin(position * div_term)
