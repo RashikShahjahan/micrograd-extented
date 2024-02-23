9d0bba9e00982c23db964d5768b21bf57d2cee97
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-02-22 19:14:49 -0500

    test

--- micrograd/layers/dropout.py
+++ micrograd/layers/dropout.py
@@ -9,7 +9,7 @@ class Dropout(nn.Module):
 
     def forward(self, x):
         if self.dropout == 1: 
-            return torch.zeros_like(x)
-        mask = (torch.rand(x.shape) > self.dropout).float()
+            return torch.zeros_like(x).to(x.device)
+        mask = (torch.rand(x.shape).to(x.device) > self.dropout).float()
         return mask * x / (1.0 - self.dropout)
 
